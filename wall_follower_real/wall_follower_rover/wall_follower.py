a66a80411a3c234f99a6d58dea6f3a68f37a90b3
ifenetangerine <irenet@mit.edu>
2026-03-08 20:15:36 -0400

    changed velocity

--- wall_follower_real/wall_follower_rover/wall_follower.py
+++ wall_follower_real/wall_follower_rover/wall_follower.py
@@ -21,7 +21,7 @@ class WallFollower(Node):
         self.declare_parameter("scan_topic", "/scan")
         self.declare_parameter("drive_topic", "/vesc/high_level/input/nav_1")
         self.declare_parameter("side", 1)
-        self.declare_parameter("velocity", 1.0)
+        self.declare_parameter("velocity", 2.0)
         self.declare_parameter("desired_distance", 1.0)
         self.declare_parameter("max_steering_angle",0.34)
         self.declare_parameter('bagging_observed_error_topic', '/observed_error') # also used for bagging purposes
