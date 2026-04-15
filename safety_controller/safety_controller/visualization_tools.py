9744250fef44d2edaf45f68b633993e9a63e7e6d
ifenetangerine <irenet@mit.edu>
2026-04-14 22:01:03 -0400

    added draw_sphere as a method in vis tools

--- safety_controller/safety_controller/visualization_tools.py
+++ safety_controller/safety_controller/visualization_tools.py
@@ -5,7 +5,7 @@ from visualization_msgs.msg import Marker
 class VisualizationTools:
 
     @staticmethod
-    def plot_line(x, y, publisher, color=(1.0, 0.0, 0.0), frame="/base_link"):
+    def plot_line(x, y, publisher, color=(1.0, 0.0, 0.0), scale=(0.1, 0.1), frame="/base_link"):
         """
         Publishes the points (x, y) to publisher
         so they can be visualized in rviz as
@@ -25,8 +25,8 @@ class VisualizationTools:
         line_strip.header.frame_id = frame
 
         # Set the size and color
-        line_strip.scale.x = 0.1
-        line_strip.scale.y = 0.1
+        line_strip.scale.x = scale[0]
+        line_strip.scale.y = scale[1]
         line_strip.color.a = 1.0
         line_strip.color.r = color[0]
         line_strip.color.g = color[1]
@@ -41,3 +41,24 @@ class VisualizationTools:
 
         # Publish the line
         publisher.publish(line_strip)
+
+    @staticmethod
+    def draw_sphere(x, y, publisher, frame, scale=(0.2, 0.2, 0.2), color=(1.0, 1.0, 0.0)):
+        """
+        Publish a marker to represent a sphere in rviz.
+        """
+        marker = Marker()
+        marker.header.frame_id = frame
+        marker.type = marker.SPHERE
+        marker.action = marker.ADD
+        marker.scale.x = scale[0]
+        marker.scale.y = scale[1]
+        marker.scale.z = scale[2]
+        marker.color.a = 1.0
+        marker.color.r = color[0]
+        marker.color.g = color[1]
+        marker.color.b = color[2]
+        marker.pose.orientation.w = 1.0
+        marker.pose.position.x = x
+        marker.pose.position.y = y
+        publisher.publish(marker)
